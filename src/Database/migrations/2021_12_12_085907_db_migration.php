d86df38b54a5ed488ffce426f5323c53bfacc31a
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-06-20 11:11:53 +0430

    Create 2021_12_12_085907_db_migration.php

---
+++ src/Database/migrations/2021_12_12_085907_db_migration.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHtaccessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('htaccesses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->unsignedInteger('status_code')->default(301);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('htaccesses');
+    }
+}
